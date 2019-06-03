f072cd7936544b96b5e44338de016061d1b2e6a1
ki <krs1231@naver.com>
2019-06-03 09:01:02 +0900

    xml 셋팅

---
+++ SoloPerson/src/main/java/com/sist/solo/MainController.java
@@ -0,0 +1,15 @@
+package com.sist.solo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("main/main.do")
+	public String mainController() {
+		
+		return "main";
+	}
+	
+}
