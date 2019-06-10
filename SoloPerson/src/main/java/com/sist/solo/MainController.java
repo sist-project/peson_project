980d5d90888408b6bcd0ac77ad8a4469da3736c7
ki <krs1231@naver.com>
2019-06-10 18:04:36 +0900

    main 수정

--- SoloPerson/src/main/java/com/sist/solo/MainController.java
+++ SoloPerson/src/main/java/com/sist/solo/MainController.java
@@ -12,14 +12,22 @@ import com.sist.vo.GenderAgeVO;
 public class MainController {
 	
 	@Autowired
-	private MainDAO maindao;
+	private MainDAO dao;
 	
 	@RequestMapping("main/main.do")
 	public String mainController() {
 		String[] city = {"서울특별시", "부산광역시" ,"대구광역시", "인천광역시" ,"광주광역시", 
 					     "대전광역시", "울산광역시", "경기도" ,"충청북도", "충청남도", "전라북도", 
 					     "전라남도", "경상남도", "경상북도", "제주특별자치도"};
-	
+		ArrayList<GenderAgeVO> list = new ArrayList<GenderAgeVO>();
+		list = (ArrayList<GenderAgeVO>) dao.genderData(city[0]);
+		
+		for (GenderAgeVO vo : list) {
+			
+		}
+		
+		
+		
 		return "main/main";
 	}
 	
