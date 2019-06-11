7108e52cd094511e354bd4b32fbe5f81dcec6877
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2019-06-11 19:31:13 +0900

    getter, setter 변경

--- SoloPerson/src/main/java/com/sist/vo/Room_categoryVO.java
+++ SoloPerson/src/main/java/com/sist/vo/Room_categoryVO.java
@@ -1,11 +1,19 @@
 package com.sist.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Room_categoryVO {
 	private int cateno;
 	private String cateimg;
+	
+	public int getCateno() {
+		return cateno;
+	}
+	public void setCateno(int cateno) {
+		this.cateno = cateno;
+	}
+	public String getCateimg() {
+		return cateimg;
+	}
+	public void setCateimg(String cateimg) {
+		this.cateimg = cateimg;
+	}
 }
--- SoloPerson/src/main/java/com/sist/vo/Room_furnitureVO.java
+++ SoloPerson/src/main/java/com/sist/vo/Room_furnitureVO.java
@@ -1,10 +1,5 @@
 package com.sist.vo;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class Room_furnitureVO {
 
 	private int productno;
@@ -17,4 +12,65 @@ public class Room_furnitureVO {
 	private String option_select;
 	private String option_color;
 	private String link;
+	
+	public int getProductno() {
+		return productno;
+	}
+	public void setProductno(int productno) {
+		this.productno = productno;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public String getProductimg() {
+		return productimg;
+	}
+	public void setProductimg(String productimg) {
+		this.productimg = productimg;
+	}
+	public String getRetailprice() {
+		return retailprice;
+	}
+	public void setRetailprice(String retailprice) {
+		this.retailprice = retailprice;
+	}
+	public String getSaleprice() {
+		return saleprice;
+	}
+	public void setSaleprice(String saleprice) {
+		this.saleprice = saleprice;
+	}
+	public String getShippingfee() {
+		return shippingfee;
+	}
+	public void setShippingfee(String shippingfee) {
+		this.shippingfee = shippingfee;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getOption_select() {
+		return option_select;
+	}
+	public void setOption_select(String option_select) {
+		this.option_select = option_select;
+	}
+	public String getOption_color() {
+		return option_color;
+	}
+	public void setOption_color(String option_color) {
+		this.option_color = option_color;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
 }
