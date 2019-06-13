99c8a7278d922b4b38c6f13603675eb5aed8e300
hesh <hesh@DESKTOP-RU1IGIL>
2019-06-13 19:49:31 +0900

    main 수정

--- SoloPerson/src/main/java/com/sist/solo/MainController.java
+++ SoloPerson/src/main/java/com/sist/solo/MainController.java
@@ -25,94 +25,61 @@ public class MainController {
 	@RequestMapping("main/main.do")
 	public String mainController(Model model) {
 		// 시도별 분포
-		String[] city = {"서울특별시", "부산광역시" ,"대구광역시", "인천광역시" ,"광주광역시", 
-					     "대전광역시", "울산광역시", "경기도" ,"충청북도", "충청남도", "전라북도", 
-					     "전라남도", "경상남도", "경상북도", "제주특별자치도"};
+		String[] city = { "서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "경기도", "충청북도", "충청남도", "전라북도",
+				"전라남도", "경상남도", "경상북도", "제주특별자치도" };
 		List<GenderAgeVO> gList = dao.genderData(city[0]);
-		
-		JSONArray arr1=new JSONArray();
+
+		JSONArray arr1 = new JSONArray();
 		for (int i = 0; i < 15; i++) {
 			JSONObject obj = new JSONObject();
 			GenderAgeVO vo = gList.get(i);
 			obj.put("age", vo.getAge());
 			obj.put("total", vo.getTotal());
-			obj.put("female",vo.getFemale());
+			obj.put("female", vo.getFemale());
 			obj.put("male", vo.getMale());
-			
+
 			arr1.add(obj);
 		}
 
 		model.addAttribute("sidoName", city[0]);
 		model.addAttribute("json1", arr1.toJSONString());
-		
-		
+
 		
 		// 개인 소비지출
-		String[] color = {"#54a0ff", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1",
-				  "#00d2d3", "#ff9ff3", "#5f27cd", "#c8d6e5", "#341f97"};
+		String[] color = { "#54a0ff", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1", "#00d2d3", "#ff9ff3", "#5f27cd",
+				"#c8d6e5", "#341f97" };
 		List<PersonalConsumptionVO1> list = dao.consumptionData1();
 		JSONArray arr2 = new JSONArray();
 		int j = 0;
-		for (PersonalConsumptionVO1 vo : list) {	
+		for (PersonalConsumptionVO1 vo : list) {
 			arr2.add(vo.getYear2018());
 			j++;
-			
+
 			if (j > 9)
 				break;
-			
+
 		}
-		System.out.println(arr2);
+		
 		JSONArray colorJson = new JSONArray();
 		for (int i = 0; i < arr2.size(); i++) {
 			colorJson.add(color[i]);
 		}
 		
-		model.addAttribute("json2", arr2);
-		model.addAttribute("colorJson", colorJson);
-		
-		
-		
-		/*String[] color = {"#54a0ff", "#feca57", "#ff6b6b", "#48dbfb", "#1dd1a1",
-						  "#00d2d3", "#ff9ff3", "#5f27cd", "#c8d6e5", "#341f97"};
-		List<PersonalConsumptionVO> vo = dao.consumptionData();
-		JSONArray arr2 = new JSONArray();
-		
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_subtotal().replaceAll(",", "")));		// 식료품,비주류 음료 소계
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_grain().replaceAll(",", "")));			// 곡물
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_bread().replaceAll(",", "")));			// 빵및떡류
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_meat().replaceAll(",", "")));			// 육류
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_aquatic_animal().replaceAll(",", "")));// 신선수산동물
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_milk_products().replaceAll(",", ""))); // 유제품 및 알
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_fruit_goods().replaceAll(",", "")));	// 과일및과일가공품
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_seasoning().replaceAll(",", "")));	 	// 조미식품
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_coffee().replaceAll(",", "")));		// 커피식품
-		arr2.add(Integer.parseInt(vo.get(0).getGroceries_juice().replaceAll(",", "")));			// 쥬스및기타음료
-
-		JSONArray colorJson = new JSONArray();
-
-		for (int i = 0; i < arr2.size(); i++) {
-			colorJson.add(color[i]);
-		}
-
-		model.addAttribute("json2", arr2);
-		model.addAttribute("colorJson", colorJson);
 		JSONArray arrLabel = new JSONArray();
+		j = 0;
 		
-		try {
-			JSONParser jsonParser = new JSONParser();
-			JSONObject jsonObj = (JSONObject) jsonParser.parse(new FileReader("C:\\git\\peson_project\\SoloPerson\\src\\main\\webapp\\main\\consumption.json"));
-			JSONArray array = (JSONArray) jsonObj.get("consumption");
-			for (int i = 0; i <array.size(); i++) {
-				JSONObject result = (JSONObject) array.get(i);
-				String name = (String) result.get("name");	
-				arrLabel.add(name);		// ["", "", ""]
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+		for (PersonalConsumptionVO1 vo : list) {
+			arrLabel.add(vo.getCol2());
+			j++;
+
+			if (j > 9)
+				break;
+
 		}
-		
-		model.addAttribute("arrLabel", arrLabel);*/
+
+		model.addAttribute("json2", arr2);
+		model.addAttribute("colorJson", colorJson);
+		model.addAttribute("arrLabel", arrLabel);
 		
 		return "main/main";
 	}
